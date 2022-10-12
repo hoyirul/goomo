d4a335fbb4e015d12e412aece205d2e02254611f
Mochammad Hairullah <irulgraspama621@gmail.com>
2022-10-12 13:45:17 +0700

    Fix ROute

--- app/Http/Controllers/Pages/HomeController.php
+++ app/Http/Controllers/Pages/HomeController.php
@@ -3,26 +3,16 @@
 namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use App\Models\UserCustomer;
-use App\Models\UserOperator;
-use App\Models\UserOwner;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
         $title = 'Motorcycles';
-        $tables = [];
+        $books = [];
         $genres = [];
-<<<<<<< HEAD
         return view('pages.home.index', compact([
             'books', 'genres', 'title'
-=======
-        return view('pages.motorcycles.index', compact([
-            'tables', 'genres', 'title'
->>>>>>> c47d67751e46b8b5a70020c7e5854d6f83c7feee
         ]));
     }
 }
