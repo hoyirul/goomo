74e1177df671a343851dc6d3331e309053eeb502
Mochammad Hairullah <irulgraspama621@gmail.com>
2022-10-12 14:19:10 +0700

    Operator

--- resources/views/pages/partials/navbar.blade.php
+++ resources/views/pages/partials/navbar.blade.php
@@ -12,7 +12,7 @@
       </ul>
       <div class="d-flex">
         @auth
-          @if (Auth::user()->role_id == 1 && Auth::user()->role_id == 2)
+          @if (Auth::user()->role_id == 1 || Auth::user()->role_id == 2)
             <a href="/operator/dashboard" class="rad-8 btn btn-dark btn-sm fs-normal font-regular p-2 px-5 float-end"><span class="fa fa-cogs me-1"></span> Dashboard</a>   
           @else
             <a href="/v2/dashboard" class="rad-8 btn btn-primary btn-sm fs-normal font-regular p-2 px-5 float-end me-2"><span class="fa fa-cogs me-1"></span> Dashboard</a>            
